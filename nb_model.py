3c4eedf25170d1d1d532ad0d5f2fc7ad2c1bbca4
rshdgvn <rasheedgavinesponga@gmail.com>
2026-03-02 02:34:01 +0800

    Naive Bayes

---
+++ nb_model.py
@@ -0,0 +1,8 @@
+from sklearn.naive_bayes import GaussianNB
+
+nb_model = GaussianNB()
+nb_model = nb_model.fit(X_train, y_train)
+
+y_pred = nb_model.predict(X_test)
+
+print(classification_report(y_pred, y_test))

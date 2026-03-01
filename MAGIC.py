1fd4c8c6da5b8def6b04eb445c53372fe6ce69ed
rshdgvn <rasheedgavinesponga@gmail.com>
2026-03-02 02:31:26 +0800

    scale dataset

--- MAGIC.py
+++ MAGIC.py
@@ -2,6 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def scale_dataset(dataframe, oversample=False):
+  X = dataframe[dataframe.columns[:-1]].values
+  y = dataframe[dataframe.columns[-1]].values
+
+  scaler = StandardScaler()
+  X = scaler.fit_transform(X)
+
+  data = np.hstack((X, np.reshape(y, (-1, 1))))
+
+  if oversample:
+    ros = RandomOverSampler()
+    X, y = ros.fit_resample(X, y)
+
+  return data, X, 
+
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
 
 df = pd.read_csv("magic04.data", names=cols)
@@ -15,3 +30,9 @@ for label in cols[:-1]:
   plt.xlabel(label)
   plt.legend()
   plt.show()
+
+train, valid, test = np.split(df.sample(frac=1), [int(0.6 * len(df)), int(0.8 * len(df))])y
+
+train_data, X_train, y_train = scale_dataset(train, oversample=True)
+valid_data, X_valid, y_valid = scale_dataset(valid, oversample=False)
+test_data, X_test, y_test = scale_dataset(test, oversample=False)
